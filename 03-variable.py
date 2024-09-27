c5b762de38e8a3777b8f3e5e43b70887b796c528
Damas Amirul Karim <damasamirulkarim@gmail.com>
2024-09-27 15:48:19 +0700

    add variable

---
+++ 03-variable.py
@@ -0,0 +1,20 @@
+# Example of Python variables
+x = 5
+y = "Hello, World!"
+z = 3.14
+is_true = True
+
+print(x)
+print(y)
+print(z)
+print(is_true)
+
+# If you want to specify the data type of a variable, this can be done with casting.
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(x, y, z)
+
+# The type() function returns the type of a variable
+print(type(x), type(y), type(z))
